7f1f39d430cfd7877ff6cfd48185a94831720f41
Yushain <73380147+YutianGG@users.noreply.github.com>
2021-06-13 01:55:45 +0800

    6/13
    Score

---
+++ CRG/Assets/script/Score.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    [Header("分數")]
+    static public float score;
+    [Header("分數文字")]
+    public Text text;
+
+    private void Awake()
+    {
+        score = 0;
+    }
+    
+    void Update()
+    {
+        AddScore(Mathf.Ceil(Time.time*0.1f));
+    }
+
+    public void AddScore(float add)
+    {
+        score += add;
+        text.text = "分數 : " + score;
+    }
+}
