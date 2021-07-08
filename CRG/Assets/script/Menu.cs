04950ab7be2c8c0c0265c5c43b63903025cded07
HSW42544869 <73380363+HSW42544869@users.noreply.github.com>
2021-07-08 15:21:37 +0800

    更新

--- CRG/Assets/script/Menu.cs
+++ CRG/Assets/script/Menu.cs
@@ -58,6 +58,11 @@ public class Menu : MonoBehaviour
     /// </summary>
     public void Back()
     {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        Time.timeScale = 1;
+    }
+    public void returngame()
+    {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
         Time.timeScale = 1;
     }
