60405063f4af5f4ab95ac8694db5600b06920bca
Yushain <73380147+YutianGG@users.noreply.github.com>
2021-07-10 11:55:42 +0800

    7/10

--- CRG/Assets/script/Camera.cs
+++ CRG/Assets/script/Camera.cs
@@ -6,6 +6,8 @@ public class Camera : MonoBehaviour
 {
     [Header("移動速度")]
     public float cspeed = 10;
+    [Header("遊戲結束")]
+    public GameObject gameclear;
 
     private Rigidbody2D rig;
 
@@ -22,6 +24,13 @@ public class Camera : MonoBehaviour
         rig.velocity = new Vector2(cspeed, 0); //水平加速度  
 
     }
-
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.name == "終點")
+        {
+            gameclear.SetActive(true);
+            cspeed = 0;
+        }
+    }
 
 }
